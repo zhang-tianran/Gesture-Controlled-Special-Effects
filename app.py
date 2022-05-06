2f2d4cfd638125aa67541b01848a64b8dedb9715
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-05-05 21:22:36 -0400

    Add draft for image stylization

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import argparse
 from collections import Counter
 from collections import deque
 
+from skimage import img_as_float32
+
 #  import pyautogui
 
 import cv2 as cv
@@ -18,6 +20,9 @@ from model import KeyPointClassifier
 from model import PointHistoryClassifier
 
 from KazuhitoTakahashiUtils.helpers import *
+
+import tensorflow as tf
+import tensorflow_hub as hub
 
 def cartoon_effect(frame): 
     # prepare color
@@ -118,6 +123,10 @@ def main():
             row[0] for row in point_history_classifier_labels
         ]
 
+    stylization_model = hub.load("model/image_stylization")
+    style_image = cv.cvtColor(cv.imread("assets/starry-night.jpeg"), cv.COLOR_BGR2RGB)
+    style_image = img_as_float32(style_image)
+
     # FPS計測モジュール ########################################################
     cvFpsCalc = CvFpsCalc(buffer_len=10)
 
@@ -200,8 +209,19 @@ def main():
                     current_mode = hand_sign_id
                 else: 
                     if (current_mode == 0): # graphic effects
-                        if (hand_sign_id == 2): 
+                        if (hand_sign_id == 2): # cartoon
                             debug_image = cartoon_effect(debug_image)
+                        elif (hand_sign_id == 0): # ghibli stylization
+                            pass
+                            #  temp_debug_image = tf.expand_dims(debug_image, 0)
+                            #  temp_debug_image = img_as_float32(temp_debug_image)
+
+                            #  temp_height = debug_image.shape[1]
+                            #  temp_width = debug_image.shape[0]
+
+                            #  temp_style_image = tf.image.resize(style_image, (temp_height, temp_width), preserve_aspect_ratio=True)
+                            #  temp_style_image = tf.expand_dims(temp_style_image, 0)
+                            #  debug_image = stylization_model(temp_debug_image, temp_style_image)[0]
 
                 if hand_sign_id == 2:  # 指差しサイン
                     point_history.append(landmark_list[8])  # 人差指座標
